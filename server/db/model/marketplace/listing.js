f9a8c08dbb7c15e7775f5675eb804ddf36efbc9d
hohohovip1 <hohohovip1@gmail.com>
2020-05-31 23:42:38 +0700

    model listing still need optimize

--- server/db/model/marketplace/listing.js
+++ server/db/model/marketplace/listing.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
   // needed information
+  photo: {
+    type: String,
+  },
   title: {
     type: String,
     require: true,
@@ -38,6 +41,77 @@ const listingSchema = new Schema({
     type: String,
   },
   //Entertainment
+  platform: {
+    type: String,
+  },
+  //Clothing & Accessories
+  size: {
+    type: String,
+  },
+  //Family
+  //Electronics
+  carrier: {
+    type: String,
+  },
+  deviceName: {
+    type: String,
+  },
+  //Hobbies
+  //Classifieds
+  //Vehicle
+  vehicleType: {
+    type: String,
+  },
+  year: {
+    type: Number,
+  },
+  make: {
+    type: String,
+  },
+  model: {
+    type: String,
+  },
+  //Home for Rent
+  rentalType: {
+    type: String,
+  },
+  numberOfBedrooms: {
+    type: String,
+  },
+  pricePerMonth: {
+    type: String,
+  },
+  rentalAddress: {
+    type: String,
+  },
+  rentalDescription: {
+    type: String,
+  },
+  //Home - Advenced Details
+  squareFeet: {
+    type: String,
+  },
+  dateAvailable: {
+    type: Date,
+  },
+  laundryType: {
+    type: String,
+  },
+  parkingType: {
+    type: String,
+  },
+  airConditioningType: {
+    type: String,
+  },
+  heatingType: {
+    type: String,
+  },
+  catFriendly: {
+    type: Boolean,
+  },
+  dogFriendly: {
+    type: Boolean,
+  },
 });
 
 module.exports = (db) => db.model('Listing', listingSchema);
