18b41bb82d5c395adcbcaeb7bcc0367ea23fcac4
Tuan Anh <ncq998@gmail.com>
2020-06-05 15:40:25 +0700

    đâs421

--- client/public/sw.js
+++ client/public/sw.js
@@ -108,8 +108,8 @@ var notGetRequests = [
   },
 ];
 
-var CACHE_STATIC_NAME = 'static-v1591338025052';
-var CACHE_DYNAMIC_NAME = 'dynamic-v1591338025052';
+var CACHE_STATIC_NAME = 'static-v1591346386711';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1591346386711';
 
 var STATIC_FILES = [
   '/',
