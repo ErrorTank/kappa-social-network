bab9144ac73650790b60d2509f55b60dafa511e4
hohohovip1 <hohohovip1@gmail.com>
2020-07-23 13:55:52 +0700

    as

--- client/public/sw.js
+++ client/public/sw.js
@@ -156,8 +156,8 @@ var notGetRequests = [
   },
 ];
 
-var CACHE_STATIC_NAME = 'static-v1595417638386';
-var CACHE_DYNAMIC_NAME = 'dynamic-v1595417638386';
+var CACHE_STATIC_NAME = 'static-v1595423796645';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1595423796645';
 
 var STATIC_FILES = [
   '/',
