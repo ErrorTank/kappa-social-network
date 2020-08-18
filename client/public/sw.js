93d7617003ff00df62443621ff0c3e72330a5d9e
hohohovip1 <hohohovip1@gmail.com>
2020-08-18 22:44:51 +0700

    wzasd

--- client/public/sw.js
+++ client/public/sw.js
@@ -172,8 +172,8 @@ var notGetRequests = [
   },
 ];
 
-var CACHE_STATIC_NAME = 'static-v1597744812612';
-var CACHE_DYNAMIC_NAME = 'dynamic-v1597744812612';
+var CACHE_STATIC_NAME = 'static-v1597765123610';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1597765123610';
 
 var STATIC_FILES = [
   '/',
--- client/react/routes/authen-routes/marketplace-route/marketplace-create-listing/create-listing-detail/cl-preview-widget/image-slider/image-slider.jsx
+++ client/react/routes/authen-routes/marketplace-route/marketplace-create-listing/create-listing-detail/cl-preview-widget/image-slider/image-slider.jsx
@@ -1,34 +1,25 @@
 import React, { Component } from 'react';
 import { LoadingInline } from './../../../../../../../common/loading-inline/loading-inline';
 import { getBase64Image } from '../../../../../../../../common/utils/file-upload-utils';
+import { ImageDisplay } from './image-display/image-display';
 
 export class ImageSlider extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      imgs: [],
-    };
+    this.state = {};
   }
-  componentWillReceiveProps = (nextProps) => {
-    if (nextProps.files !== this.props.files) {
-      const { files } = this.props;
-      let imgs = [];
-      files.forEach((file) => {
-        getBase64Image(file.file).then((base64Image) => {
-          imgs.push(base64Image);
-        });
-      });
-      console.log(imgs);
-      this.setState({ imgs });
-    }
+  getImageSrc = (file) => {
+    getBase64Image(file.file).then((base64Image) => {
+      return base64Image;
+    });
   };
   render() {
     const { files } = this.props;
-    const { imgs } = this.state;
-    // console.log(files[0]);
+    console.log(this.state);
     return (
       <div className='image-slider'>
-        {!!imgs.length && imgs.map((each) => <img src={each} alt='' />)}
+        {!!files.length &&
+          files.map((file) => <ImageDisplay key={file.fileID} file={file} />)}
       </div>
     );
   }
