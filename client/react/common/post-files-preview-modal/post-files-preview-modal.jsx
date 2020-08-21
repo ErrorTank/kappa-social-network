6c54123b07b809f5c90638d90eda888cc209e965
Tuan Anh <ncq998@gmail.com>
2020-08-21 18:33:28 +0700

    dada31

--- client/react/common/post-files-preview-modal/post-files-preview-modal.jsx
+++ client/react/common/post-files-preview-modal/post-files-preview-modal.jsx
@@ -18,11 +18,28 @@ export const postFilesPreviewModal = {
 
 class PostFilesPreviewModal extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            focusFileID: props.focusFileID
+        }
+    }
+
     render() {
         let {files, focusFileID} = this.props;
+        let currentFile = files.find(each => each._id === focusFileID);
         return (
             <div className="post-files-preview-modal">
+                <div className="file-panel">
+                    <div className="file-container">
+                         <div className="file-wrapper">
 
+                         </div>
+                    </div>
+                </div>
+                <div className="describe-panel">
+
+                </div>
             </div>
         );
     }
