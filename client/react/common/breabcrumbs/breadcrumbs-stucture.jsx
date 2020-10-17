57001cc54f5dae927e7b51bf19c2003b33f99140
hohohovip1 <hohohovip1@gmail.com>
2020-10-17 18:01:10 +0700

    asdasdasd

--- client/react/common/breabcrumbs/breadcrumbs-stucture.jsx
+++ client/react/common/breabcrumbs/breadcrumbs-stucture.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import omit from 'lodash/omit';
+import reverse from 'lodash/reverse';
 
 const marketplaceStucture = {
   childrens: [
@@ -13,6 +15,11 @@ const marketplaceStucture = {
             {
               url: '/marketplace/112344455778999abbccdeff',
               label: <span className='label'>Xe hơi/Xe tải</span>,
+            },
+
+            {
+              url: '/marketplace/112344455778999abbccdfff',
+              label: <span className='label'>Xe máy</span>,
             },
           ],
         },
@@ -35,4 +42,28 @@ const marketplaceStucture = {
       ],
     },
   ],
+};
+
+let buildingRecursive = (structure, pathname, result) => {
+  if (pathname === structure.url) {
+    result = [...result, omit(structure, ['childrens'])];
+    return result;
+  }
+  if (structure.childrens) {
+    for (const str of structure.childrens) {
+      result = buildingRecursive(str, pathname, result);
+      if (result.length) {
+        if (structure.hasOwnProperty('label')) {
+          result = [...result, omit(structure, ['childrens'])];
+        }
+
+        return result;
+      }
+    }
+  }
+};
+
+export const createBreadcrumbBuilder = (pathname, result = []) => {
+  let final = buildingRecursive(marketplaceStucture, pathname, result);
+  return reverse(final);
 };
--- client/react/common/breabcrumbs/breadcrumbs.jsx
+++ client/react/common/breabcrumbs/breadcrumbs.jsx
@@ -1,1 +1,33 @@
-import React from 'react';
+import { each } from 'lodash';
+import React, { Component } from 'react';
+import { customHistory } from '../../routes/routes';
+import { createBreadcrumbBuilder } from './breadcrumbs-stucture';
+
+export class Breadcrumbs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    let { location } = customHistory;
+    const breadcrumbsArray = createBreadcrumbBuilder(location.pathname);
+    return (
+      <div className='breadcrumbs-container'>
+        {breadcrumbsArray.length > 1 && (
+          <div className='breadcrumbs'>
+            {breadcrumbsArray.map((e) => (
+              <div
+                className='breadcrumbs-item'
+                key={each.url}
+                onClick={() => customHistory.push(each.url)}
+              >
+                {each.label}
+              </div>
+            ))}
+          </div>
+        )}
+        {this.props.children}
+      </div>
+    );
+  }
+}
