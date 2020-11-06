bf5361b9f82d473cd65c71e3a5414077d396dc47
hohohovip1 <hohohovip1@gmail.com>
2020-11-06 23:25:54 +0700

    asdasd

--- client/react/routes/authen-routes/marketplace-route/listing-full-display/listing-full-display.jsx
+++ client/react/routes/authen-routes/marketplace-route/listing-full-display/listing-full-display.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { listingApi } from './../../../../../api/common/listing-api';
 import { PageTitle } from './../../../../common/page-title/page-title';
 import { ImageSlider } from './../marketplace-create-listing/create-listing-detail/cl-preview-widget/image-slider/image-slider';
-
+import { Button } from './../../../../common/button/button';
+import classnames from 'classnames';
 class ListingFullDisplay extends Component {
   constructor(props) {
     super(props);
@@ -17,13 +18,156 @@ class ListingFullDisplay extends Component {
   }
   render() {
     const { listing } = this.state;
+    console.log(listing);
     return (
       <PageTitle title={'Listing'}>
         <div className='listing-full-display'>
           <div className='picture-display-section'>
             <ImageSlider files={listing.files} />
           </div>
-          <div className='listing-all-info-section'></div>
+          <div className='listing-all-info-section'>
+            <div className='info-display-wrapper'>
+              {/* <div className='main-info-wrapper'>
+                <div className={classnames('info-title')}>
+                  {type === 'item'
+                    ? title
+                      ? title
+                      : 'Tiêu đề'
+                    : type === 'vehicle'
+                    ? state.make || state.year || state.model
+                      ? `${state.year} ${state.make} ${state.model}`
+                      : 'Tiêu đề'
+                    : state.homeType
+                    ? state.homeType
+                    : 'Tiêu đề'}
+                </div>
+                <div
+                  className={classnames('info-price', {
+                    'on-mouse': hoverArr === 'price',
+                  })}
+                >
+                  {price ? price : 'Giá'}
+                </div>
+                <div className='info-time-position'>
+                  Đã niêm yết vài giây trước tại {state.location || '...'}{' '}
+                </div>
+              </div>
+
+              <div className='button-section-wrapper'>
+                <Button
+                  disabled={true}
+                  className={classnames('facebook-button long')}
+                >
+                  <i className='fab fa-facebook-messenger'></i>
+                  <span>Nhắn tin</span>
+                </Button>
+                <Button
+                  disabled={true}
+                  className={classnames('facebook-button')}
+                >
+                  <i className='fas fa-bookmark'></i>
+                </Button>
+                <Button
+                  disabled={true}
+                  className={classnames('facebook-button')}
+                >
+                  <i className='fas fa-share'></i>
+                </Button>
+                <Button
+                  disabled={true}
+                  className={classnames('facebook-button')}
+                >
+                  <i className='fas fa-ellipsis-h'></i>
+                </Button>
+              </div>
+
+              <div className='addition-info-wrapper'>
+                <div className='addition-info-header'>Chi tiết</div>
+                <div className='addition-info-body'>
+                  <div
+                    className={classnames('addition-wrapper', {
+                      'on-mouse': hoverArr === 'category',
+                    })}
+                  >
+                    {category && (
+                      <div className='addition-info'>
+                        <div className='addition-info-type'>Tình trạng</div>
+                        <div className='addition-info-content'>
+                          {condition ? condition : '___'}
+                        </div>
+                      </div>
+                    )}
+                    {this.additionInfo.map((each) => {
+                      return (
+                        state[each.name] && (
+                          <div className='addition-info'>
+                            <div className='addition-info-type'>
+                              {each.title}
+                            </div>
+                            <div className='addition-info-content'>
+                              {state[each.name]}
+                            </div>
+                          </div>
+                        )
+                      );
+                    })}
+                  </div>
+                  <div
+                    className={classnames('decription-wrapper', {
+                      'on-mouse': hoverArr === 'decription',
+                    })}
+                  >
+                    {state.decription
+                      ? state.decription
+                      : 'Phần mô tả sẽ hiển thị tại đây'}
+                  </div>
+                  <div
+                    className={classnames('location-wrapper', {
+                      'on-mouse': hoverArr === 'location',
+                    })}
+                  >
+                    <div className='example-map'>
+                      <img
+                        src={
+                          'https://localhost:2000/assets/images/others/best-google-maps-plugins.png'
+                        }
+                        alt=''
+                      />
+                    </div>
+                    <div className='location-info'>
+                      {state.location ? state.location : 'Vị trí...'}
+                    </div>
+                    <div className='addition-text'>
+                      Đây là chỉ vị trí gần đúng
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='seller-info-wrapper gray-filter'>
+                <div className='seller-info-header'>Thông tin về người bán</div>
+                <div className='seller-info-body'>
+                  <div className='seller-avatar-wrapper'>
+                    <Avatar user={user} />
+                  </div>
+                  <div className='seller-name-wrapper'>
+                    <div className='user-name'>{user.basic_info.username}</div>
+                  </div>
+                </div>
+              </div> */}
+            </div>
+
+            {/* <div className='send-message-wrapper'>
+              <div className='send-message-header gray-filter'>
+                <i className='fab fa-facebook-messenger'></i>
+                <div className='send-message-title'>
+                  Gửi tin nhắn cho người bán
+                </div>
+              </div>
+              <div className='message-example'>Mặt hàng này còn chứ</div>
+              <div className='send-message-demo gray-filter'>Gửi</div>
+            </div> */}
+          </div>
           <div className='blank-section'></div>
         </div>
       </PageTitle>
