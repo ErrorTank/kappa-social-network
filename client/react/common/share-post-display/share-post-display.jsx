7d00829df72f2f6d578daadaec8931d2fcfd275b
hohohovip1 <hohohovip1@gmail.com>
2021-01-19 23:27:56 +0700

    small fix  share listing post

--- client/react/common/share-post-display/share-post-display.jsx
+++ client/react/common/share-post-display/share-post-display.jsx
@@ -1,40 +1,44 @@
-import React, {Component} from 'react';
-import {postApi} from "../../../api/common/post-api";
-import {LoadingInline} from "../loading-inline/loading-inline";
-import {PostBox} from "../post-box/post-box";
+import React, { Component } from 'react';
+import { postApi } from '../../../api/common/post-api';
+import { LoadingInline } from '../loading-inline/loading-inline';
+import { PostBox } from '../post-box/post-box';
 
 export class SharePostDisplay extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            post: null
-        }
-        if(props.postID){
-            postApi.getPostByID(props.postID)
-                .then(post => this.setState({post, loading: false}))
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      post: null,
+    };
+    if (props.postID) {
+      postApi
+        .getPostByID(props.postID)
+        .then((post) => this.setState({ post, loading: false }));
     }
+  }
 
-    render() {
-        let {loading, post} = this.state;
-        return (
-            <div className="share-post-display">
-                {loading ? (
-                    <LoadingInline/>
-                ) : post ? (
-                    <PostBox
-                        isPreview={true}
-                        post={post}
-                    />
-                ) : (
-                    <div className="post-not-existed">
-                        <div className="pne-title">Không khả dụng</div>
-                        <div className="pne-content">Nội dung chia sẻ đã bị xóa hoặc không tồn tại!</div>
-                    </div>
-                )}
+  render() {
+    let { loading, post } = this.state;
+    console.log(post);
+    return (
+      <div className='share-post-display'>
+        {loading ? (
+          <LoadingInline />
+        ) : post ? (
+          <PostBox
+            isPreview={true}
+            post={post}
+            listing={post.listing && post.listing}
+          />
+        ) : (
+          <div className='post-not-existed'>
+            <div className='pne-title'>Không khả dụng</div>
+            <div className='pne-content'>
+              Nội dung chia sẻ đã bị xóa hoặc không tồn tại!
             </div>
-        );
-    }
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
